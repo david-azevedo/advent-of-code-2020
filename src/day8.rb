403146fac774292d490df8e976ae0c813664bce3
David Azevedo <davidazevedo1996@hotmail.com>
2020-12-08 13:13:31 +0000

    Day 8 Solution cleaned up

---
+++ src/day8.rb
@@ -0,0 +1,52 @@
+# frozen_string_literal: true
+
+lines = File.readlines("../data/#{__FILE__.split('.')[0]}.txt")
+
+# Run the program until we either reach the end or find a repeated instruction
+def run_program(lines)
+  acc = 0
+  offset = 0
+  offsets_seen = {}
+  # Small otimization for Part 2 only storing jmp and nop instructions that might be incorrect
+  instructions_to_change = []
+
+  loop do 
+    return acc, 'terminated' if offset >= lines.length
+
+    return acc, instructions_to_change if offsets_seen[offset]
+
+    offsets_seen[offset] = 1
+    ins, value = lines[offset].split(' ')
+
+    case ins
+    when 'acc'
+      acc += value.to_i
+    when 'jmp'
+      instructions_to_change.push(offset)
+      offset += value.to_i
+      next
+    when 'nop'
+      instructions_to_change.push(offset)
+    end
+    # Next instruction
+    offset += 1
+  end
+end
+
+part1, to_change = run_program(lines)
+
+puts "Part1: #{part1}"
+
+# Part 2: Iterating through all the seen instructions in the first run and editing one by one
+# and checking if that swap makes the program terminate. If yes we have found the solution
+to_change.each do |index|
+  new_lines = lines.map(&:clone)
+  new_lines[index][0..2] = 'nop' if lines[index].start_with?('jmp')
+  new_lines[index][0..2] = 'jmp' if lines[index].start_with?('nop')
+
+  part2, result = run_program(new_lines)
+  if result == 'terminated'
+    puts "Part2: #{part2}"
+    break
+  end
+end
