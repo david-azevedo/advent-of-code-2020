f7de7a4465fe010698b76d4950f3e26d84d9145f
David Azevedo <davidazevedo1996@hotmail.com>
2020-12-08 16:54:06 +0000

    Clean up

--- src/day8.rb
+++ src/day8.rb
@@ -1,13 +1,14 @@
 # frozen_string_literal: true
+
 require 'set'
 
 lines = File.readlines("../data/#{__FILE__.split('.')[0]}.txt")
 
 # Apply the instruction to a given pair of accumulator and offset
-def run_instruction(instruction, acc, offset)
-  ins, value = instruction.split(' ') # "acc +14" -> ["acc", "+14"]
+def run_instruction(line, acc, offset)
+  instruction, value = line.split(' ') # "acc +14" -> ["acc", "+14"]
 
-  case ins
+  case instruction
   when 'acc'
     [acc + value.to_i, offset + 1]
   when 'jmp'
@@ -21,42 +22,41 @@ end
 def run_program(lines)
   acc = 0
   offset = 0
-  # Keeping track of all the offsets we have seen so far
-  offsets_seen = Set.new
+  offsets_seen = Set.new # Keeping track of all the offsets we have seen so far
 
   loop do
     # If we reach the end they return the accumulator and 'terminated' status
-    return acc, 'terminated' if offset >= lines.length || offset < 0
+    return acc, 'terminated' if offset >= lines.length || offset.negative?
 
     # If we see a repeat instruction (loop found) return the accumulator
     # and the list of seen instructions
     return acc, offsets_seen.to_a if offsets_seen.include?(offset)
 
-    # Add this offset to our list of seen offsets
-    offsets_seen << offset
+    offsets_seen << offset # Add this offset to our list of seen offsets
 
     # Update the accumulator and offset by running the instruction
     acc, offset = run_instruction(lines[offset], acc, offset)
   end
 end
 
-part1, to_change = run_program(lines)
-
-puts "Part1: #{part1}"
-
 # Part 2: Iterating through all the seen instructions in the first run and
 # editing one by one and checking if that swap makes the program terminate.
 # If so we have found the solution.
-to_change.each do |index|
-  # This is copying the array if we simply do 'new_lines = lines' then lines
-  # will also be changed when we change new_lines
-  new_lines = lines.map(&:clone)
-  new_lines[index][0..2] = 'nop' if lines[index].start_with?('jmp')
-  new_lines[index][0..2] = 'jmp' if lines[index].start_with?('nop')
+def fix_bad_instruction(lines, to_change)
+  to_change.each do |index|
+    # This is copying the array if we simply do 'new_lines = lines' then lines
+    # will also be changed when we change new_lines
+    new_lines = lines.map(&:clone)
+    new_lines[index][0..2] = 'nop' if lines[index].start_with?('jmp')
+    new_lines[index][0..2] = 'jmp' if lines[index].start_with?('nop')
 
-  part2, result = run_program(new_lines)
-  if result == 'terminated'
-    puts "Part2: #{part2}"
-    break
+    part2, result = run_program(new_lines)
+    return part2 if result == 'terminated'
   end
 end
+
+part1, to_change = run_program(lines)
+part2 = fix_bad_instruction(lines, to_change)
+
+puts "Part1: #{part1}"
+puts "Part2: #{part2}"
