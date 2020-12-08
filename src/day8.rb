b04aa318660efed456e478fc88947255fa131485
David Azevedo <davidazevedo1996@hotmail.com>
2020-12-08 13:17:54 +0000

    Comment solution for better understanding

--- src/day8.rb
+++ src/day8.rb
@@ -7,10 +7,11 @@ def run_program(lines)
   acc = 0
   offset = 0
   offsets_seen = {}
-  # Small otimization for Part 2 only storing jmp and nop instructions that might be incorrect
+  # Small otimization for Part 2 only storing jmp and nop instructions
+  # that might be incorrect
   instructions_to_change = []
 
-  loop do 
+  loop do
     return acc, 'terminated' if offset >= lines.length
 
     return acc, instructions_to_change if offsets_seen[offset]
@@ -37,9 +38,12 @@ part1, to_change = run_program(lines)
 
 puts "Part1: #{part1}"
 
-# Part 2: Iterating through all the seen instructions in the first run and editing one by one
-# and checking if that swap makes the program terminate. If yes we have found the solution
+# Part 2: Iterating through all the seen instructions in the first run and
+# editing one by one and checking if that swap makes the program terminate.
+# If so we have found the solution.
 to_change.each do |index|
+  # This is copying the array if we simply do 'new_lines = lines' then lines
+  # will also be changed when we change new_lines
   new_lines = lines.map(&:clone)
   new_lines[index][0..2] = 'nop' if lines[index].start_with?('jmp')
   new_lines[index][0..2] = 'jmp' if lines[index].start_with?('nop')
