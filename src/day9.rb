81bc8615d5b1ec2dc031c7555baf75c98afc2a2b
David Azevedo <davidazevedo1996@hotmail.com>
2020-12-09 18:20:15 +0000

    Clean up solution and make it more efficient

--- src/day9.rb
+++ src/day9.rb
@@ -1,51 +1,39 @@
 # frozen_string_literal: true
 
-require 'set'
-
 lines = File.readlines("../data/#{__FILE__.split('.')[0]}.txt").map(&:to_i)
 
 def find_pair_sum(numbers, num)
-  pairs = []
-  numbers = numbers.to_set
   numbers.each do |number|
     if numbers.include?(num - number)
-      pairs.push([num, num - number]) unless num == num - number
+      return true unless num == num - number
     end
   end
-  pairs
+  false
+end
+
+def find_invalid(lines, preamble)
+  lines.length.times do |index|
+    next if index < preamble
+
+    next if find_pair_sum(lines[index - preamble, index], lines[index])
+
+    return lines[index]
+  end
 end
 
 def contiguous_sum(lines, num)
-  lines.length.times do |index|
-    aux = index + 1
-    sub_array = [lines[index]]
-    sum = lines[index]
-    while aux < lines.length
-      sum += lines[aux]
-      sub_array.push(lines[aux])
+  start = 0
+  finish = 1
+  while finish < lines.length
+    sum = lines[start..finish].inject(:+)
+    return lines[start..finish] if sum == num
 
-      return sub_array if sum == num
-
-      break if sum > num
-
-      aux += 1
-    end
+    start += 1 if sum > num
+    finish += 1 if sum < num
   end
 end
 
-preamble = 25
-part1 = 0
-
-lines.length.times do |index|
-  next if index < preamble
-
-  pairs = find_pair_sum(lines[index - preamble, index], lines[index])
-  if pairs.empty?
-    part1 = lines[index]
-    break
-  end
-end
-
+part1 = find_invalid(lines, 25)
 sub_array = contiguous_sum(lines, part1)
 part2 = sub_array.min + sub_array.max
 
