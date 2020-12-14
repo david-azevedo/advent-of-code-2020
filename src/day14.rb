14f31da62d5275f277ac1a1461f30ae5cd744925
David Azevedo <davidazevedo1996@hotmail.com>
2020-12-14 20:14:50 +0000

    Clean up

--- src/day14.rb
+++ src/day14.rb
@@ -5,19 +5,8 @@ lines = File.readlines("../data/#{__FILE__.split('.')[0]}.txt")
 def convert_floating_to_mem(floating)
   # Generate all possible combinations for the floating points
   perms = [0, 1].repeated_permutation(floating.count('X')).to_a
-  result = []
   # For each combination generate the correct number
-  perms.each do |perm|
-    value = floating
-    perm.each do |bit|
-      # This works because sub substitutes only the first occurence
-      # while gsub substitutes all occurences
-      value = value.sub('X', bit.to_s)
-    end
-    result.append(value.to_i(2))
-  end
-
-  result
+  perms.map { |perm| perm.map { |bit| floating.sub('X', bit.to_s) } }
 end
 
 def apply_mask_v2(mask, value)
@@ -46,40 +35,33 @@ def apply_mask_v1(mask, value)
   result.to_i(2)
 end
 
-bitmask = ''
-memory = {}
+def solve(lines, part1)
+  bitmask = ''
+  memory = {}
 
-# Part 1
-lines.each do |line|
-  if line.start_with?('mask')
-    bitmask = line.split('=')[1].strip
-  elsif line.start_with?('mem')
-    address, value = line.split('=').map(&:strip)
-    address = address.match(/\[([0-9]*)\]/).captures[0].to_i
-    value = apply_mask_v1(bitmask, value)
-    memory[address] = value
-  end
-end
-
-part1 = memory.values.inject(:+)
-bitmask = ''
-memory = {}
-
-# Part 2
-lines.each do |line|
-  if line.start_with?('mask')
-    bitmask = line.split('=')[1].strip
-  elsif line.start_with?('mem')
-    address, value = line.split('=').map(&:strip)
-    address = address.match(/\[([0-9]*)\]/).captures[0].to_i
-    addresses = apply_mask_v2(bitmask, address)
-    addresses.each do |adr| # Write multiple memory addresses at once
-      memory[adr] = value.to_i
+  lines.each do |line|
+    if line.start_with?('mask')
+      bitmask = line.split('=')[1].strip
+    elsif line.start_with?('mem')
+      address, value = line.split('=').map(&:strip)
+      address = address.match(/\[([0-9]*)\]/).captures[0].to_i
+      if part1
+        value = apply_mask_v1(bitmask, value)
+        memory[address] = value
+      else
+        addresses = apply_mask_v2(bitmask, address)
+        addresses.each do |adr| # Write multiple memory addresses at once
+          memory[adr] = value.to_i
+        end
+      end
     end
   end
+
+  memory.values.inject(:+)
 end
 
-part2 = memory.values.inject(:+)
+part1 = solve(lines, true)
+part2 = solve(lines, false)
 
 puts "Part1: #{part1}"
 puts "Part2: #{part2}"
